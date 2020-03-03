c227d207b9cfa0e99de9c38f392ef5546347ee2c
trountrymcgrath <trountrymcgrath@student.bridgew.edu>
2020-03-03 10:38:00 -0500

    Midterm: attempt at Exam class structure

---
+++ src/comp152/midterm/Exam.java
@@ -0,0 +1,32 @@
+package comp152.midterm;
+
+import java.util.ArrayList;
+
+public class Exam {
+    private ArrayList<Question> allQuestions;
+    private int version;
+    }
+
+    public DisplayExam(){
+        allQuestions = new ArrayList<Question>();
+        System.out.println(+ Question +);
+    }
+
+    public Exam(ArrayList<starterQuestions> Exam) {
+        points = new Assessment;
+        allQuestions = starterQuestions;
+        allQuestions = points(10);
+
+    }
+
+    public int correctExam() {
+        var totalPoints = points.random.int;
+        var success = points.add(totalPoints);
+        return success;
+    }
+
+    public takeExam() {
+        DisplayExam();
+        correctExam();
+    }
+}
